8f092e2aa0a4a71cb726921d79f011e211f71870
ipiano <andrew.d.stelter@gmail.com>
2022-03-04 16:25:41 -0600

    Add dummy test with gtest

---
+++ cmake/FindGTest.cmake
@@ -0,0 +1,13 @@
+find_package(GTest NO_MODULE QUIET)
+
+if(NOT TARGET GTest_FOUND)
+  add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../submodules/googletest
+                   googletest)
+
+  add_library(GTest::gtest ALIAS gtest)
+  add_library(GTest::gtest_main ALIAS gtest_main)
+  add_library(GTest::gmock ALIAS gmock)
+  add_library(GTest::gmock_main ALIAS gmock_main)
+
+  set(GTest_FOUND TRUE)
+endif()
