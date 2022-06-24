465315ecf054cd9f95a569d492ce0ff5b4717976
ipiano <andrew.d.stelter@gmail.com>
2022-06-24 11:52:22 -0500

    Move constants into a namespace

--- src/visualizer/constants.h
+++ src/visualizer/constants.h
@@ -2,12 +2,24 @@
 
 #include <functional>
 
-const int ESCAPE     = 27;
-const int ITEMS      = 4;
+namespace constants
+{
+namespace keys
+{
+const int ESCAPE = 27;
+const int ITEMS  = 4;
+const int RESET  = 'r';
+}
+
+namespace colors
+{
 const float FREE[]   = {1, 1, 1};
 const float ACCESS[] = {1, 0, 0};
 const float HOLD[]   = {0, 1, 0};
 const float TEXT[]   = {0.5, 0.5, 0.5};
 const float BLACK[]  = {0, 0, 0};
-typedef unsigned char byte;
-typedef std::function<void()> semfunction;
+}
+}
+
+using byte        = unsigned char;
+using semfunction = std::function<void()>;
