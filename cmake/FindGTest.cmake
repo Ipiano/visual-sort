b07f23171a76c15d03ef4d7d3ed0ec4fc1ab0bcb
Andrew Stelter <andrew.d.stelter@gmail.com>
2022-07-16 15:19:41 -0500

    Add cmake formatting standard

--- cmake/FindGTest.cmake
+++ cmake/FindGTest.cmake
@@ -1,13 +1,12 @@
 find_package(GTest NO_MODULE QUIET)
 
 if(NOT TARGET GTest_FOUND)
-  add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../submodules/googletest
-                   googletest)
+    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../submodules/googletest googletest)
 
-  add_library(GTest::gtest ALIAS gtest)
-  add_library(GTest::gtest_main ALIAS gtest_main)
-  add_library(GTest::gmock ALIAS gmock)
-  add_library(GTest::gmock_main ALIAS gmock_main)
+    add_library(GTest::gtest ALIAS gtest)
+    add_library(GTest::gtest_main ALIAS gtest_main)
+    add_library(GTest::gmock ALIAS gmock)
+    add_library(GTest::gmock_main ALIAS gmock_main)
 
-  set(GTest_FOUND TRUE)
+    set(GTest_FOUND TRUE)
 endif()
--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,14 +1,12 @@
 function(add_sanitizer sanitizer)
-  add_library(fsanitize_${sanitizer} INTERFACE)
-  target_compile_options(fsanitize_${sanitizer}
-                         INTERFACE "SHELL:-fsanitize=${sanitizer}")
-  target_link_options(fsanitize_${sanitizer} INTERFACE
-                      "SHELL:-fsanitize=${sanitizer}")
+    add_library(fsanitize_${sanitizer} INTERFACE)
+    target_compile_options(fsanitize_${sanitizer} INTERFACE "SHELL:-fsanitize=${sanitizer}")
+    target_link_options(fsanitize_${sanitizer} INTERFACE "SHELL:-fsanitize=${sanitizer}")
 
-  add_library(fsanitize::${sanitizer} ALIAS fsanitize_${sanitizer})
+    add_library(fsanitize::${sanitizer} ALIAS fsanitize_${sanitizer})
 endfunction()
 
 if(NOT TARGET fsanitize::address)
-  add_sanitizer(address)
-  add_sanitizer(undefined)
+    add_sanitizer(address)
+    add_sanitizer(undefined)
 endif()
