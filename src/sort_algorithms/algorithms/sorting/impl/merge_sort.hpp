77b1ba15ac3d58c399644e0dbeb871683c2a3210
Andrew Stelter <andrew.d.stelter@gmail.com>
2022-07-17 12:55:50 -0500

    Move scratch-space implementation out of mergesort
    This will be useful for other sort algorithms that need background scratch
    space, like bucket sorts.

--- src/sort_algorithms/algorithms/sorting/impl/merge_sort.hpp
+++ src/sort_algorithms/algorithms/sorting/impl/merge_sort.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "algorithms/sorting/impl/scratch_space.hpp"
+
 #include <algorithm>
 #include <cassert>
 #include <iterator>
@@ -14,47 +16,6 @@ using std::move;
 using std::swap;
 
 template <class Iterator> using value_type_t = typename iterator_traits<Iterator>::value_type;
-
-// Appropriately does a swap to next or emplace depending on how full it
-// is
-template <class T> class scratch_space
-{
-  public:
-    using iterator = typename std::vector<T>::iterator;
-
-    scratch_space(std::size_t capacity)
-    {
-        m_buffer.reserve(capacity);
-        m_next = m_buffer.begin(); // NOLINT (cppcoreguidelines-prefer-member-initializer) This must be initialized after reserve()
-    }
-
-    void add(T& value)
-    {
-        if (m_next == m_buffer.end())
-        {
-            // Emplace should never result in a resize because we always
-            // reserve enough space for the final merge of the sort
-            assert(m_buffer.size() < m_buffer.capacity());
-
-            m_buffer.emplace_back(move(value));
-        }
-        else
-        {
-            swap(value, *m_next);
-        }
-        ++m_next;
-    }
-
-    iterator begin() { return m_buffer.begin(); }
-    iterator end() { return m_next; }
-
-    void clear() { m_next = m_buffer.begin(); }
-    std::size_t size() { return static_cast<std::size_t>(distance(begin(), end())); }
-
-  private:
-    std::vector<T> m_buffer;
-    iterator m_next;
-};
 
 template <class RandomIt, class Compare>
 void merge_sort(const RandomIt begin, const RandomIt end, Compare compare, scratch_space<value_type_t<RandomIt>>& scratch)
