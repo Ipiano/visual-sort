e36cd0f6e607af4f7a3a6b92d89360eb01c16008
Andrew Stelter <andrew.stelter@cnhind.com>
2024-10-16 06:34:40 -0500

    Fix captured binding
    Not an issue with GCC, definitely an issue for Clang

--- src/visualizer/main.cpp
+++ src/visualizer/main.cpp
@@ -107,8 +107,8 @@ int main(int argc, char* argv[])
                 auto [sort_function, draw_function] = args.get_sort_and_draw_function();
 
                 visualizer.start(data_set, sort_function,
-                                 [draw_function](const std::vector<SortVisualizer::Item>& items, int max_value,
-                                                 glut::Coordinate viewport_origin, glut::Size viewport_size)
+                                 [draw_function = draw_function](const std::vector<SortVisualizer::Item>& items, int max_value,
+                                                                 glut::Coordinate viewport_origin, glut::Size viewport_size)
                                  {
                                      draw_function(items, max_value, viewport_origin, viewport_size);
                                      std::this_thread::sleep_for(chrono::milliseconds(1));
